e7cce9197114c14f8f72606be559b70d3f9c1c62
Tanveer Ansari <mtanveer20u@gmail.com>
2024-11-24 00:46:34 +0100

    Remove node modules folder

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import './App.css';
-import Footer from './footer';
-import Header from './header';
-import MainContent from './main';
+import React from "react";
+import "./App.css";
+import Footer from "./footer";
+import Header from "./header";
+import MainContent from "./main";
+import { AboutUs } from "./Pages/AboutUs";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" Component={MainContent} />
+          <Route path="aboutUs" Component={AboutUs} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/header.jsx
+++ src/header.jsx
@@ -1,51 +1,78 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header>
-    <nav className="navbar navbar-expand-lg navbar-light shadow-sm" style={{backgroundColor : '#ceffb8'}}>
+      <nav
+        className="navbar navbar-expand-lg navbar-light shadow-sm"
+        style={{ backgroundColor: "#ceffb8" }}
+      >
         <div className="container">
-            {/* Logo */}
-            <img className="mb-3" src="/imfralogo.png" alt="Logo" style={{ height: '70px' }} />
+          {/* Logo */}
+          <img
+            className="mb-3"
+            src="/imfralogo.png"
+            alt="Logo"
+            style={{ height: "70px" }}
+          />
 
-            {/* Navigation Links */}
-            <div
-                className="collapse navbar-collapse justify-content-end"
-                id="navbarNav"
-                style={{ fontFamily: 'Jost, sans-serif', fontWeight: 400 }}
+          {/* Navigation Links */}
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarNav"
+            style={{ fontFamily: "Jost, sans-serif", fontWeight: 400 }}
+          >
+            <ul
+              className="navbar-nav"
+              style={{ gap: "15px", display: "flex", alignItems: "center" }}
             >
-                <ul className="navbar-nav" style={{ gap: '15px', display: 'flex', alignItems: 'center' }}>
-                    <li className="nav-item">
-                        <a className="nav-link nav-text" href="#"    >
-                            Home
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link nav-text" href="#"    >
-                            Gallery
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link nav-text" href="#"    >
-                            About Us
-                        </a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="btn btn-success text-white ms-lg-3" href="#" style={{ padding: '5px 15px' }}>
-                            Join Us!
-                        </a>
-                    </li>
-                    <li className="nav-item ms-4">
-                        <button className="btn btn-outline-success d-flex align-items-center" style={{ borderWidth: '2px', fontSize: '14px', padding: '5px 10px', fontWeight: 600 }}>
-                            <i className="bi bi-globe me-2" style={{ fontSize: '16px' }}></i>DE
-                        </button>
-                    </li>
-                </ul>
-            </div>
+              <li className="nav-item">
+                <Link className="nav-link nav-text" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link nav-text" href="#">
+                  Gallery
+                </a>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link nav-text" to="aboutUs">
+                  About Us
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="btn btn-success text-white ms-lg-3"
+                  href="#"
+                  style={{ padding: "5px 15px" }}
+                >
+                  Join Us!
+                </a>
+              </li>
+              <li className="nav-item ms-4">
+                <button
+                  className="btn btn-outline-success d-flex align-items-center"
+                  style={{
+                    borderWidth: "2px",
+                    fontSize: "14px",
+                    padding: "5px 10px",
+                    fontWeight: 600,
+                  }}
+                >
+                  <i
+                    className="bi bi-globe me-2"
+                    style={{ fontSize: "16px" }}
+                  ></i>
+                  DE
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
-    </nav>
-</header>
-
+      </nav>
+    </header>
   );
 }
 
