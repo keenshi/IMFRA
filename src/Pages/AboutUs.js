9d5f012164b3ed2f8eb84d209124ef9dd84fa688
Tanveer Ansari <mtanveer20u@gmail.com>
2024-11-24 00:48:12 +0100

    Added About us page and git ignore

---
+++ src/Pages/AboutUs.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const AboutUs = () => {
+  return (
+    <div className="container">
+      <h1>This is About Us page. All details about it goes here... </h1>
+    </div>
+  );
+};
