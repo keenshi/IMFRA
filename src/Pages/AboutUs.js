b2119054f82b01b75b3cbc97abc40b8f6d5c2844
Tanveer Ansari <mtanveer20u@gmail.com>
2024-11-25 00:01:36 +0100

    about us page content

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -1,9 +1,36 @@
 import React from "react";
+import "./../Styles/style.css";
 
 export const AboutUs = () => {
   return (
-    <div className="container">
-      <h1>This is About Us page. All details about it goes here... </h1>
+    <div className="container content-container alert alert-light text-justify">
+      <h2 className="text-center margin-bottom-normal">Vision</h2>
+      <p className="font-size-17">
+        Our vision is to establish a strong Indian Muslim community base for
+        those residing in the Hessen region and neighboring areas.
+      </p>
+      <p className="font-size-17">
+        Our goals are to promote a sense of brotherhood and bring individuals
+        and families together. Through a variety of events such as Islamic,
+        social, and welfare activities, along with virtual and in-person
+        meet-ups and family get-togethers, we aim to unite and strengthen our
+        community locally.
+      </p>
+      <p className="font-size-17">
+        Our objective is to bring everyone together and promote community
+        development programs that benefit all members.
+      </p>
+      <p className="font-size-17">
+        By connecting with each other, we can enhance our social lives and
+        create a sense of belonging to our community. Together, we are strong
+        and can achieve great things.{" "}
+      </p>
+      <p className="font-size-17">
+        We value the participation of all Indian Muslim professionals, students,
+        and leaders from all walks of life, and are committed to promoting
+        brotherhood and community development while maintaining transparency in
+        all our activities.
+      </p>
     </div>
   );
 };
