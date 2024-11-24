466d649cd32c869b3c4525a0ee992ca9263a3d7b
Masood Ahmed Mohiuddin <81741563+keenshi@users.noreply.github.com>
2024-11-24 22:23:07 +0100

    made changes

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -1,9 +1,8 @@
-import React from "react";
 
 export const AboutUs = () => {
   return (
     <div className="container">
-      <h1>This is About Us page. All details about it goes here... </h1>
+      <h1>This is About Us page. All details about it goes here. </h1>
     </div>
   );
 };
