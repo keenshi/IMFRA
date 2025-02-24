ec8ebcaf809dfe14df12c3dbadd6e41caadb98ed
Tanveer Ansari <mtanveer20u@gmail.com>
2025-02-24 01:14:09 +0100

    integrate Stripe payment

--- src/Pages/Donate.js
+++ src/Pages/Donate.js
@@ -1,10 +1,28 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import DonateBanner from "../Donate.png";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./../Styles/style.css";
+import Form from "react-bootstrap/Form";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePublishableKey =
+  "pk_live_51M21TPKPPt2IOFXI1b4X1KKVbsqWWrC15gHESAnDhBF3w6V6ZtHlEVMDhxsDnqhbJPakdJcNiT6wPKJFiq3WzpoT00LS19yite";
+const studentLink = "https://buy.stripe.com/3cs9Bi1iDcoc5B69AG";
+const professionalLink = "https://buy.stripe.com/8wM8xe1iD9c08Ni6os";
+const membershipPlusLink = "https://buy.stripe.com/dR67taaTdcoc4x28wB";
+const customLink = "";
 
 function Donate() {
   const navigate = useNavigate();
+
+  const [selectedPlan, setSelectedPlan] = useState("");
+  const [paymentURL, setpaymentURL] = useState(professionalLink);
+
+  const handlePayment = async () => {
+    const sessionUrl = `https://donate.stripe.com/test_9AQ02Bdnhbk7epacMM`;
+    window.location.href = paymentURL;
+  };
 
   return (
     <div
@@ -63,32 +81,38 @@ function Donate() {
         </p>
       </div>
 
-      <div
-        className="shadow"
-        style={{
-          borderRadius: "30px",
-          backgroundColor: "#ffffff",
-          padding: "40px",
-          marginTop: "30px",
-        }}
-      >
-        <h2
-          style={{
-            fontWeight: "600",
-            textAlign: "center",
-            marginBottom: "30px",
-          }}
-        >
-          How to Donate?
-        </h2>
+      <div className="donate-container shadow">
+        <h2 className="donate-header">Support Our Cause</h2>
         <p
           style={{ fontSize: "16px", lineHeight: "1.8", textAlign: "justify" }}
         >
-          Donating is simple and secure. Click the "Donate Now" button below to
-          contribute online. Alternatively, you can visit our office or contact
-          us directly to arrange your donation. Every contribution, big or
-          small, makes a meaningful impact.
+          Your donation helps us continue our mission.
         </p>
+        <div className="doante-options-container">
+          <Form>
+            <Form.Check
+              className="margin-bottom-normal"
+              label="Support as a student member and contribute to our initiatives at a discounted rate. (5 €)"
+              name="group1"
+              type="radio"
+              id="student-radio"
+            />
+            <Form.Check
+              defaultChecked
+              className="margin-bottom-normal"
+              label="Join as a professional member to help sustain and expand our mission. (15 €)"
+              name="group1"
+              type="radio"
+              id="professional-radio"
+            />
+            <Form.Check
+              label="Custom donation amount to contribute as much as you wish."
+              name="group1"
+              type="radio"
+              id="customeAmount-radio"
+            />
+          </Form>
+        </div>
         <div style={{ textAlign: "center", marginTop: "20px" }}>
           <button
             style={{
@@ -101,7 +125,8 @@ function Donate() {
               fontSize: "16px",
             }}
             onClick={() => {
-              navigate("/payment");
+              // navigate("/payment");
+              handlePayment();
             }}
           >
             Donate Now
